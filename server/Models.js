6fea285cbb386548f1c2742c65b4d0743045cd6a
Sejin Park <sejinpark0209@gmail.com>
2020-07-12 20:26:00 -0700

    get result form  getHotelPhoto query

--- server/Models.js
+++ server/Models.js
@@ -1,7 +1,8 @@
 const db = require('../database-postgresql');
 
 function getHotelPhotos(hotelId, callback) {
-  const q = 'SELECT * FROM hotels WHERE id=0';
+  console.log(hotelId)
+  const q = `SELECT t_imageUrl, caption FROM traveler_photos INNER JOIN hotels ON traveler_photos.hotel_id = hotels.id WHERE traveler_photos.hotel_id = ${hotelId}`;
   db.query(q, callback);
 }
 
